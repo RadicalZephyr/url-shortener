36365efcdde28eea25447c79a3b64428c1bb2246
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-31 15:59:59 -0400

    Move src path to accomodate cljs files

--- project.clj
+++ project.clj
@@ -3,9 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [compojure "1.2.1"]]
   :plugins      [[lein-ring "0.8.13"]
                  [lein-cljsbuild "1.0.3"]]
-  :ring    {:handler url-shortener.core/app})
+  :ring    {:handler url-shortener.core/app}
+  :cljsbuild {:builds [{:source-paths ["src/cljs"]
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
