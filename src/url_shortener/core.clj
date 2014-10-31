746bfec44c592b0ec15361b22bc7d46dfbb22bf1
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-31 12:46:06 -0400

    Actually shorten urls correctly!!
    Needed to store the short url as a string...

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -8,19 +8,19 @@
 
 (defn shorten-url [url]
   (let [shortened (mod (hash url) 1000)]
-    (swap! url-mapping assoc shortened url)
+    (swap! url-mapping assoc (str shortened) url)
     shortened))
 
 (defroutes routes
   (GET "/" [] (response/redirect "/index.html"))
   (POST "/" [url]
         (let [short (shorten-url url)]
-          (str "We shortened your url to: " short)))
+          (str "We shortened your url to: " short @url-mapping)))
   (route/resources "/")
   (GET "/:url-id" [url-id]
        (let [url (get @url-mapping url-id)]
          (if url
            (response/redirect url)
-           "<h1>No redirect was found.</h1>"))))
+           (str "<h1>No redirect was found.</h1>" @url-mapping)))))
 
 (def app (wrap-params routes))
