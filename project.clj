7422be3469a63e3204eb216127888e2d60491cc3
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-31 11:50:52 -0400

    Add ring dependencies

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.2.1"]])
+                 [compojure "1.2.1"]]
+  :plugins      [[lein-ring "0.8.13"]]
+  :ring    {:handler url-shortener.core/app})
