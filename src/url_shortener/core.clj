9b5b61e01da7cf239bc8abd4acf21c8cbfc0903c
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-31 12:33:20 -0400

    Add parameter middleware

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,17 +1,19 @@
 (ns url-shortener.core
   (:require [ring.util.response :as response]
-            [compojure.core :refer :all]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [GET POST defroutes]]
             [compojure.route :as route]))
-
 
 (def url-mapping (atom {}))
 
-(defroutes app
+(defroutes routes
   (GET "/" [] (response/redirect "/index.html"))
-  (POST "/" params (str "Got a url" params))
+  (POST "/" {params :params} (str "Got a url" (pr-str params)))
   (route/resources "/")
   (GET "/:url-id" [url-id]
        (let [url (get @url-mapping url-id)]
          (if url
            (response/redirect url)
            "<h1>No redirect was found.</h1>"))))
+
+(def app (wrap-params routes))
