9f6b3dfc56af5b1cb43d152a99a8da11a05490dc
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-31 11:50:58 -0400

    Get things basically working

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,7 +1,17 @@
 (ns url-shortener.core
-  (:require [compojure.core :refer :all]
+  (:require [ring.util.response :as response]
+            [compojure.core :refer :all]
             [compojure.route :as route]))
 
+
+(def url-mapping (atom {"abcde" "http://www.google.com/search?q=thebestkittenever"
+                        }))
+
+
 (defroutes app
-  (GET "/" [] "<h1>Hello World</h1>")
-  (route/not-found "<h1>Page not found</h1>"))
+  (GET "/" [] "<h1>Hello Url Shortening World</h1>")
+  (GET "/:id" [id]
+       (let [url (get @url-mapping id)]
+         {:status 302
+          :headers {"Location" url}
+          :body ""})))
