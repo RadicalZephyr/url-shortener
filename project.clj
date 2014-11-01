afe5229a81e042b66393647bba97a7abc3b679d3
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-01 18:37:19 -0400

    Add an explicit ring dependency, and add ring-edn

--- project.clj
+++ project.clj
@@ -8,7 +8,9 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
+                 [ring "1.3.1"]
                  [compojure "1.2.1"]
+                 [fogus/ring-edn "0.2.0"]
                  [cljs-ajax "0.3.3"]]
 
   :plugins      [[lein-ring "0.8.13"]
