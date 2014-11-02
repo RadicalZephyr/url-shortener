86daa6d5185c5a18f2f12465e9dda2957e43dfce
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-01 20:30:23 -0400

    Fix FIXME's in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject url-shortener "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple URL shortener"
+  :url "http://www.github.com/RadicalZephyr/url-shortener"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
